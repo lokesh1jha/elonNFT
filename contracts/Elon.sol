e05c9ef6a74beee85641903f21728a41bf0c7c6a
lokeshjha <lokesh1jha@gmai.com>
2024-02-26 01:04:21 +0530

    elon nft deployed

---
+++ contracts/Elon.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9; 
+
+import "hardhat/console.sol";
+
+contract ElonNFT {
+    constructor() {
+        console.log("This is my Elon Musk NFT contract!!");
+    }
+}
